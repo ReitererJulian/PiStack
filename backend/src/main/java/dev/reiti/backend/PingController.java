f864c4070e535c3662ebf84d594fe04d1cdae596
ReitererJulian <230011@o365.htl-leoben.at>
2026-05-03 12:19:08 +0200

    Initial commit

---
+++ backend/src/main/java/dev/reiti/backend/PingController.java
@@ -0,0 +1,15 @@
+package dev.reiti.backend;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PingController{
+
+    @RequestMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
